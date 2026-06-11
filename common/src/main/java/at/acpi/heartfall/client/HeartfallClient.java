d0dd87e01688157f83aa21caf2f03921ff447234
Lívia <k@kyu.re>
2026-06-11 05:35:56 -0300

    chore: remove old architectury multi-project structure

--- common/src/main/java/at/acpi/heartfall/client/HeartfallClient.java
+++
@@ -1,9 +0,0 @@
-package at.acpi.heartfall.client;
-
-import at.acpi.heartfall.client.registry.HeartfallEntityRenderers;
-
-public class HeartfallClient {
-    public static void init() {
-        HeartfallEntityRenderers.registerEntityRenderers();
-    }
-}
--- common/src/main/java/at/acpi/heartfall/client/renderer/HeartShardEntityRenderState.java
+++
@@ -1,8 +0,0 @@
-package at.acpi.heartfall.client.renderer;
-
-import net.minecraft.client.renderer.entity.state.EntityRenderState;
-
-public class HeartShardEntityRenderState  extends EntityRenderState {
-    public float scale, rotation, bob;
-    public int alpha;
-}
