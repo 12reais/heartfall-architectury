08761171800a2e5515fe34d07836c4b1355b5350
lívia <k@kyu.re>
2026-09-07 23:38:09 -0300

    refactor(build): use configured minecraft version ranges instead of hardcoded values

--- build.fabric.gradle.kts
+++ build.fabric.gradle.kts
@@ -6,7 +6,7 @@ platform {
 	loader = "fabric"
 	dependencies {
 		required("minecraft") {
-			versionRange("1.21.11" until "26.3")
+			versionRange(project.configured("minecraft")!!.min())
 		}
 		required("fabric-entity-events-v1") {
 			slug(modrinth = "fabric-api")
--- build.neoforge.gradle.kts
+++ build.neoforge.gradle.kts
@@ -6,7 +6,7 @@ platform {
 	loader = "neoforge"
 	dependencies {
 		required("minecraft") {
-			versionRange("1.21.11" until "26.3")
+			versionRange(project.configured("minecraft")!!.min())
 		}
 		required("neoforge") {
 			requires atLeast "1"
